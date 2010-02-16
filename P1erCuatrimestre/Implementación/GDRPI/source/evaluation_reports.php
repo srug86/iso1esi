805da5cd27d35142a52511a850bbd42dc80ec9ce
swarfeex <swarfeex@6d970b46-e02f-11de-9043-17b7bd24f792>
2010-02-16 14:28:33 +0000

    Arreglados algunos errores

--- P1erCuatrimestre/Implementación/GDRPI/source/evaluation_reports.php
+++ P1erCuatrimestre/Implementación/GDRPI/source/evaluation_reports.php
@@ -35,9 +35,12 @@ function save_expert_report() {
   
   $data = serialize($_POST['sec']);
   $end = $_POST['end'] == 1 ? ", state='done'" : "";
-  $_mysql->
-    sql("UPDATE eval_reports SET data='$data'$end WHERE id={$_POST['id']}");
+  $id = $_POST['id'];
+  $_mysql->sql("UPDATE eval_reports SET data='$data'$end WHERE id=$id");
   $_SESSION['msg'] = "Evaluación guardada con éxito";
+
+  if ($end) evaluated_expert($id);
+  
   header("Location: ./");
 }
 
@@ -45,7 +48,7 @@ function end_report() {
   global $_mysql, $_user;
 
   $id = $_POST['id'];
-  if ($_user['type'] == "expert")
+  if ($_user['type'] == "expert") 
     $sql = "UPDATE eval_reports SET state='done' WHERE id=$id";
   else  {
     $ppid = substr($id, 2, strpos($id, "p", 2)-2);
@@ -59,6 +62,21 @@ function end_report() {
   }
   
   $_mysql-> sql($sql);
+  if ($_user['type'] == "expert") evaluated_expert($id);
   $_SESSION['msg'] = "Evaluación finalizada con éxito";
+}
+
+function evaluated_expert($id) {
+  global $_mysql;
+  
+  $r = $_mysql->
+    row("SELECT COUNT(DISTINCT er.state) AS n, ep1.ppid, ep1.pid "
+        ."FROM eval_reports er, `experts-projects` ep1, "
+        ."`experts-projects` ep2 WHERE ep1.rid=$id AND "
+        ."ep1.ppid=ep2.ppid AND ep1.pid=ep2.pid AND ep2.rid=er.id");
+
+  if ($r['n'] == 1)
+    $_mysql->sql("UPDATE projects SET state='evaluated_experts' "
+                 ."WHERE pid={$r['ppid']} AND id={$r['pid']}");    
 }
 ?>
